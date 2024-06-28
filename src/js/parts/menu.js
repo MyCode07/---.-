59db70679483aa5539fa58a3dfed15c9bd17025c
MyCode07 <erem19992015@mail.ru>
2024-06-28 19:52:08 +0300

    finish layout

--- src/js/parts/menu.js
+++ src/js/parts/menu.js
@@ -1,27 +1,17 @@
-import { isMobile } from '../utils/isMobile.js';
 import { lockPadding, unLockPadding } from '../utils/lockPadding.js';
-import { gsap } from 'gsap'
-
 
 const burger = document.querySelector('.header__burger');
-const header = document.querySelector('.header');
-const menu = document.querySelector('.menu');
+const menu = document.querySelector('.header__menu');
+const hero = document.querySelector('.hero');
 
 if (burger) {
     burger.addEventListener('click', (e) => {
-        if (catalogMenu.classList.contains('_open')) {
-            header.classList.remove('_active');
-            catalogMenu.classList.remove('_open');
-
-            openCatalogBtns.forEach(btn => {
-                btn.classList.remove('_active');
-            })
-        }
-
         burger.classList.toggle('_active');
-        header.classList.toggle('_active');
         menu.classList.toggle('_open');
-        document.body.classList.toggle('_noscroll');
+
+        if (hero) {
+            hero.classList.toggle('_active');
+        }
 
         if (menu.classList.contains('_open')) {
             lockPadding();
@@ -29,134 +19,25 @@ if (burger) {
         else {
             unLockPadding();
         }
-
-
     })
 }
 
-// menu arrow buttom
-const arrow = `<button><svg width="14" height="14" viewBox="0 0 14 14">
-<path d="M0 7H7M14 7H7M7 7V0V14" stroke-width="1.2"/>
-</svg>
-</button>`;
+const menuLinks = document.querySelectorAll('.header__menu nav a');
+if (menuLinks) {
+    menuLinks.forEach(link => {
+        link.addEventListener('click', (e) => {
+            if (menu.classList.contains('_open')) {
+                unLockPadding();
 
-// add menu summenu opener button
-const submenuList = document.querySelectorAll('nav ul li');
-if (submenuList.length) {
-    submenuList.forEach(li => {
-        const submenu = li.querySelector('ul');
-        const link = li.querySelector('a');
+                burger.classList.remove('_active');
+                menu.classList.remove('_open');
 
-        if (submenu) {
-            link.insertAdjacentHTML('afterend', arrow);
-            const btn = li.querySelector('button');
-
-            if (btn) {
-                btn.addEventListener('click', function () {
-                    toggleMenu(li)
-                })
-            }
-        }
-    })
-
-    function toggleMenu(item) {
-        const menu = item.closest('ul');
-        const menuItems = menu.querySelectorAll('li');
-
-        if (!item.hasAttribute('data-open')) {
-            const openitem = menu.querySelector('li[data-open]');
-            if (openitem) {
-                openitem.removeAttribute('data-open')
-            }
-
-            menuItems.forEach(item => {
-                item.removeAttribute('data-open')
-            })
-
-            item.setAttribute('data-open', 'open')
-        }
-        else {
-            if (!item.closest('.catalog-menu')) {
-                item.removeAttribute('data-open')
-            }
-            else {
-                if (isMobile.any()) {
-                    item.removeAttribute('data-open')
+                if (hero) {
+                    hero.classList.remove('_active');
                 }
             }
-        }
-    }
-}
 
-
-
-
-const openCatalogBtns = document.querySelectorAll('[data-open-catalog]');
-const catalogMenu = document.querySelector('.catalog-menu')
-
-if (openCatalogBtns.length) {
-    openCatalogBtns.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            e.preventDefault();
-
-            if (openServicesBtn.classList.contains('_active')) {
-                header.classList.remove('_active');
-                openServicesBtn.classList.remove('_active');
-            }
-
-            if (menu.classList.contains('_open')) {
-                header.classList.remove('_active');
-                menu.classList.remove('_open');
-                burger.classList.remove('_active');
-            }
-
-            catalogMenu.classList.toggle('_open');
-            btn.classList.toggle('_active');
-            header.classList.toggle('_active');
-
-            if (btn.classList.contains('_active')) {
-                lockPadding();
-            }
-            else {
-                unLockPadding();
-            }
         })
     })
+
 }
-
-
-const openServicesBtn = document.querySelector('.header__services');
-if (openCatalogBtns) {
-    openServicesBtn.addEventListener('click', (e) => {
-        openServicesBtn.classList.toggle('_active');
-        header.classList.toggle('_active');
-
-        if (openServicesBtn.classList.contains('_active')) {
-            lockPadding();
-        }
-        else {
-            unLockPadding();
-        }
-    })
-}
-
-document.addEventListener('click', function (e) {
-    let targetEl = e.target;
-
-    if ((!targetEl.classList.contains('header__services') && !targetEl.closest('.header__services')) && openServicesBtn.classList.contains('_active')) {
-        openServicesBtn.classList.remove('_active');
-        header.classList.remove('_active');
-        unLockPadding();
-    }
-
-    if (!targetEl.hasAttribute('data-open-catalog') && !targetEl.closest('.catalog-menu') &&
-        !targetEl.classList.contains('catalog-menu') && catalogMenu.classList.contains('_open')) {
-        catalogMenu.classList.remove('_open');
-        header.classList.remove('_active');
-        unLockPadding();
-
-        openCatalogBtns.forEach(btn => {
-            btn.classList.remove('_active');
-        })
-    }
-})
--- src/js/parts/sliders.js
+++ src/js/parts/sliders.js
@@ -1,40 +1,27 @@
 import Swiper from 'swiper';
-import {Pagination, Autoplay } from 'swiper/modules';
+import { Autoplay } from 'swiper/modules';
 
 const sliders = document.querySelectorAll('.swiper');
 if (sliders.length) {
     sliders.forEach(slider => {
         const section = slider.closest('section');
-        let prev = section.querySelector('.prev')
-        let next = section.querySelector('.next')
-        let pagination = section.querySelector('.pagination')
 
-        if (slider.closest('.product-slider')) {
-            new Swiper(slider, {
-                modules: [Autoplay, Pagination],
-                slidesPerView: 1,
-                pagination: {
-                    el: pagination,
-                    clickable: true,
+        new Swiper(slider, {
+            modules: [Autoplay],
+            slidesPerView: 'auto',
+            loop: true,
+            autoplay: {
+                delay: 3000,
+                pauseOnMouseEnter: true
+            },
+            breakpoints: {
+                300: {
+                    spaceBetween: 10
                 },
-                autoplay: {
-                    delay: 4000,
-                },
-            })
-        }
-
-        if (slider.closest('.page-about')) {
-            new Swiper(slider, {
-                modules: [Pagination],
-                slidesPerView: 1,
-                spaceBetween: 10,
-                pagination: {
-                    el: pagination,
-                    clickable: true,
-                },
-            })
-        }
+                769: {
+                    spaceBetween: 20
+                }
+            }
+        })
     })
 }
-
-
