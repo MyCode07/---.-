1c087224705606f659508ee31931f0da59eddfef
MyCode07 <erem19992015@mail.ru>
2024-06-29 09:00:34 +0300

    add to top btn

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import { accorden } from "./static/accordeon.js";
 import { stickyHeader } from "./parts/header.js";
+import { toTop } from "./static/to-top.js";
 
 import "./parts/sliders.js";
 import "./parts/menu.js";
 
 accorden();
 stickyHeader();
+toTop();
